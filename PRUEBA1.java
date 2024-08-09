7189bfb6a8254d5b427aad969ed7e496fa699686
cadaba25 <carlosdavid.barreda@gmail.com>
2024-08-09 10:39:52 -0600

    prueba1

---
+++ PRUEBA1.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package prueba1;
+
+/**
+ *
+ * @author David Barreda
+ */
+public class PRUEBA1{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        int Var = 20;
+        double Dec = 10.5:
+        
+        
+    }
+    
+
+}
+
+
+/* Tema Varibles
+ Variable = contenedor de datos 
+
+Entero- int,short,long
+decimal-double
+Caracteres-String
+
+SINTAXIS:
+
+Tipo de datos identificador(nombre de variable);
+
+
+
+
+*/
+
+
+
